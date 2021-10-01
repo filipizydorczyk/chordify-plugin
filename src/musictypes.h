4b9838a9ad8e61d9d6f68d7df716933f59fab9be
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2021-10-02 00:34:59 +0200

    Preapared structures for core implementation

---
+++ src/musictypes.h
@@ -0,0 +1,30 @@
+enum MusicVariant
+{
+    MAJOR = 0,
+    MINOR = 1
+};
+
+enum Sound
+{
+    C = 0,
+    CIS = 1,
+    D = 2,
+    DIS = 3,
+    E = 4,
+    F = 5,
+    FIS = 6,
+    G = 7,
+    GIS = 8,
+    A = 9,
+    AIS = 10,
+    B = 11,
+
+};
+
+class Scale
+{
+public:
+    Scale(Sound key, MusicVariant variant) {}
+    ~Scale() {}
+    std::vector<note_name> getChordByNote(Sound note) {}
+};
